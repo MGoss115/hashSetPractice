c933e0aa93d96ef577b8f3b724eee8f438f01a27
mligon115 <mligon115@yahoo.com>
2022-05-11 11:54:52 -0500

    added linkedlist to Bucket class

--- Bucket.java
+++ Bucket.java
@@ -2,4 +2,10 @@ import java.util.LinkedList;
 
 public class Bucket {
 
+    private LinkedList<Integer> container;
+
+    public Bucket(){
+        container = new LinkedList<Integer>();
+    }
+
 }
