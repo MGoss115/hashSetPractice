79ffe920e74e11360b631e458e7ac8a26aa2c351
mligon115 <mligon115@yahoo.com>
2022-05-11 12:23:59 -0500

    added insert, delete, and exist to LinkedList Bucket Class

--- Bucket.java
+++ Bucket.java
@@ -8,4 +8,20 @@ public class Bucket {
         container = new LinkedList<Integer>();
     }
 
+    public void insert(Integer key){
+        int index = this.container.indexOf(key);
+        if(index == -1){
+            this.container.addFirst(key);
+        }
+    }
+
+    public void delete(Integer key){
+        this.container.remove(key);
+    }
+
+    public boolean exists(Integer key){
+        int index = this.container.indexOf(key);
+        return(index != -1);
+    }
+
 }
