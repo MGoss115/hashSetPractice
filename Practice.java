3b3202c1416c94f536720d8eb5b2d1044d64c1b4
mligon115 <mligon115@yahoo.com>
2022-05-12 18:00:01 -0500

    completed Isomorphic challenge

--- Practice.java
+++ Practice.java
@@ -82,4 +82,28 @@ public class Practice {
         }
         return null;
     }
+
+    // Given two strings s and t, determine if they are isomorphic.
+
+    public boolean isIsomorphic(String s, String t){
+
+        Map<Character, Character> map = new HashMap<>();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) != t.charAt(i)){
+                    return false;
+                }
+            }else{
+                if(map.containsValue(t.charAt(i)))
+                    return false;
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
 }
