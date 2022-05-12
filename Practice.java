7784c1650fe0d44929d170566109db91b99fed84
mligon115 <mligon115@yahoo.com>
2022-05-12 11:25:10 -0500

    two sum completed w/hashmap

--- Practice.java
+++ Practice.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Practice {
@@ -63,5 +64,22 @@ public class Practice {
             index++;
         }
         return result;
+    }
+
+    // Given an array of integers nums and an integer target, return indices of the
+    // two numbers such that they add up to target.
+
+    public int[] twoSum(int[] nums, int target){
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[] {map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
